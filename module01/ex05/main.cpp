2817ad662334df15d8a0c3f8d9d56174491ac7cd
Marco pellegrini <mpellegr@MacBook-Air-di-Marco.local>
2025-01-16 18:44:00 +0200

    added ex05 module01

---
+++ module01/ex05/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpellegr <mpellegr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/16 17:42:37 by mpellegr          #+#    #+#             */
+/*   Updated: 2025/01/16 18:39:17 by mpellegr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int main() {
+	Harl harl;
+
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+	harl.complain("kgfubj");
+	return 0;
+}
